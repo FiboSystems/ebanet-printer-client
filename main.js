e98fdc6337fcf10dd73e8036274f08b27ba4ca66
Misael Cázares <misael.upg@gmail.com>
2021-10-24 22:11:58 -0500

    New build and deploy process.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const { app, BrowserWindow, nativeImage, Tray, Menu, ipcMain } = require('electron')
 const path = require('path')
 const api = require("./api");
+const { autoUpdater } = require('electron-updater');
 if (require('electron-squirrel-startup')) return;
 
 if (handleSquirrelEvent()) {
@@ -135,6 +136,9 @@ function createWindow () {
     return false;
   })
   api(mainWindow);
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
 // This method will be called when Electron has finished
@@ -154,4 +158,15 @@ app.whenReady().then(() => {
 // code. You can also put them in separate files and require them here.
 ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
 });
